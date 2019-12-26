2673b1c10a13bd0b63521d0985607d4fb1bb097e
vv13 <zwhvv13@gmail.com>
2019-12-26 18:42:45 +0800

    initial

---
+++ src/ts/popup/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Store } from 'webext-redux';
+import PopupApp from './containers/PopupApp';
+
+const store = new Store();
+
+store.ready().then(() => {
+	debugger
+	ReactDOM.render(
+		<Provider store={store}>
+			<PopupApp />
+		</Provider>
+		, document.getElementById('popup-root'));
+});
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const ExtensionReloader = require('webpack-extension-reloader');
+const locateContentScripts = require('./utils/locateContentScripts');
+
+const sourceRootPath = path.join(__dirname, 'src');
+const contentScriptsPath = path.join(sourceRootPath, 'ts', 'contentScripts');
+const distRootPath = path.join(__dirname, 'dist');
+const nodeEnv = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+const webBrowser = process.env.WEB_BROWSER ? process.env.WEB_BROWSER : 'chrome';
+
+const contentScripts = locateContentScripts(contentScriptsPath);
+
+const extensionReloader = nodeEnv === "watch" ? new ExtensionReloader({
+	port: 9128,
+	reloadPage: true,
+	entries: {
+		background: 'background',
+		extensionPage: ['popup', 'options', 'newtab'],
+		contentScript: Object.keys(contentScripts),
+	}
+}) : () => {this.apply = () => {}};
+
+module.exports = {
+	watch: nodeEnv === 'watch',
+    entry: {
+        background: path.join(sourceRootPath, 'ts', 'background', 'index.ts'),
+        options: path.join(sourceRootPath, 'ts', 'options', 'index.tsx'),
+        popup: path.join(sourceRootPath, 'ts', 'popup', 'index.tsx'),
+        newtab: path.join(sourceRootPath, 'ts', 'newtab', 'index.tsx'),
+        ...contentScripts,
+    },
+    output: {
+        path: distRootPath,
+        filename: '[name].js',
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+    },
+    module: {
+        rules: [
+            { test: /\.(js|ts|tsx)?$/, loader: "awesome-typescript-loader", exclude: /node_modules/ },
+        ]
+    },
+    plugins: [
+        new CheckerPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.join(sourceRootPath, 'html', 'options.html'),
+            inject: 'body',
+            filename: 'options.html',
+            title: 'Octopus - Options Page',
+            chunks: ['options'],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(sourceRootPath, 'html', 'popup.html'),
+            inject: 'body',
+            filename: 'popup.html',
+            title: 'Octopus - Popup Page',
+            chunks: ['popup'],
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(sourceRootPath, 'html', 'newtab.html'),
+            inject: 'body',
+            filename: 'newtab.html',
+            title: 'Octopus - NewTab Page',
+            chunks: ['newtab'],
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: path.join(sourceRootPath, 'assets'),
+                to: path.join(distRootPath, 'assets'),
+                test: /\.(jpg|jpeg|png|gif|svg)?$/,
+            },
+            {
+                from: path.join(sourceRootPath, 'manifest.json'),
+                to: path.join(distRootPath, 'manifest.json'),
+                toType: 'file',
+            }
+        ]),
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(nodeEnv),
+            'WEB_BROWSER': JSON.stringify(webBrowser),
+		}),
+		extensionReloader,
+    ],
+}
+
+if (nodeEnv === 'production') {
+    module.exports.plugins.push(new CleanWebpackPlugin(distRootPath, { verbose: true, dry: false }));
+}
